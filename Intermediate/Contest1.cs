765b84060ed84ab0afe48ae60588f8fcf55ac39b
sdadi <satishdad@gmail.com>
2024-03-21 20:06:39 +0530

    Intermediate project with Contest-1

---
+++ Intermediate/Contest1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate
+{
+    /// <summary>Contest-1 on 21 March 2024</summary>
+    internal class Contest1
+    {
+    }
+}
